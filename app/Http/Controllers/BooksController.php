99f61391db31a5286eda6f593c792278bb7ac62d
arcoders <anonismaa@gmail.com>
2017-11-22 19:20:10 +0100

    BooksController, métodos para Datatables

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Yajra\DataTables\Contracts\DataTable;
 
 class BooksController extends Controller
 {
-    //
+
+    public function index()
+    {
+        return view('books.list');
+    }
+
+    public function booksData()
+    {
+        $model = Book::with('libraries', 'author')->withTrashed();
+        $actions = 'books.datatables.actions';
+
+        return DataTables::eloquent($model)
+                            ->addColumn('action', $actions)
+                            ->make(true);
+    }
+
 }
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    {{ Html::style('css/app.css') }}
+
+    @stack('stylesheets')
+
+    <script>
+        window.Laravel = <?php echo json_encode([
+            'csrfToken' => csrf_token()
+        ]); ?>
+    </script>
+
+</head>
+<body>
+
+<div id="app">
+    <div class="container">@yield('content')</div>
+</div>
+
+<script src="/js/app.js">
+
+@stack('scripts')
+
+</body>
+</html>
